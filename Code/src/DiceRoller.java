3af2d31562d209e9738123f0643c97a1c500fc7b
jinli <jinli@berkeley.edu>
2019-01-30 21:38:35 -0800

    add back tracking: DiceRoller.java and PhoneNumberLetter.java

---
+++ Code/src/DiceRoller.java
@@ -0,0 +1,41 @@
+public class  DiceRoller{
+
+
+    public static void main(String[] args){
+        diceRolls(2);
+    }
+
+    public static void diceRolls(int dice){
+        //array need to be reachable by all recursive calls
+        Integer[] chosen = new Integer[dice];
+        diceHelper(dice, chosen, 0);
+    }
+
+    public static void diceHelper(int dice, Integer[] chosen, int level){
+        if (dice == 0){
+            //base case, print out chosen array
+            for(int i=0; i< level; ++i){
+                System.out.print(chosen[i]);
+            }
+            System.out.println("");
+        }else{
+            //some dice left to roll
+            //handle one die in current recursive call
+
+            //for each value that die could have
+            for (int i = 1; i<=6; i++){
+                //	choose
+                chosen[level] = i;
+                //	explore
+                diceHelper(dice-1, chosen, ++level);
+                //	unchoose
+                --level;
+                chosen[level] = null;
+            }
+        }
+    }
+
+
+
+
+}
---
+++ Code/src/PhoneNumberLetters.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jin on 1/30/2019.
+ */
+public class PhoneNumberLetters {
+
+    public static  final String[] LETTERS =
+            new String[]{ "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public static void main(String[] args){
+        letterCombinations("234");
+    }
+
+    public static List<String> letterCombinations(String digits) {
+        int len = digits.length();
+        String[] chosen = new String[len];
+        List<String> results = new ArrayList<>();
+        String[] letters = new String[len];
+        for(int i=0; i<len; i++){
+            int number = Integer.parseInt(String.valueOf(digits.charAt(i)));
+            letters[i] = LETTERS[number];
+        }
+
+        letterCombinationHelper(len, letters, chosen,  results, 0);
+
+
+        return results;
+    }
+
+    public static void letterCombinationHelper(int len, String[] letters, String[] chosen, List<String> results,
+                                               int level) {
+        if (level == len) {
+            StringBuilder sb = new StringBuilder();
+            for (String str : chosen) {
+                sb.append(str);
+            }
+            String str = sb.toString();
+            System.out.print(str);
+            System.out.println("");
+            results.add(str);
+        } else {
+            String letter = letters[level];
+            int size = letter.length();
+            for (int i = 0; i < size; i++) {
+                //	choose
+                String str = String.valueOf(letter.charAt(i));
+                chosen[level] = str;
+                //	explore
+                letterCombinationHelper(len, letters, chosen, results, ++level);
+                    //	unchoose
+                    --level;
+                    chosen[level] = null;
+                }
+
+            }
+
+    }
+}
+
+
